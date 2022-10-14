10597d177e66f7d276b9245c4541d5a6d494d661
Temurbek <rajabovtemurbek04@gmail.com>
2022-10-14 11:04:46 +0500

    Initial commit

---
+++ TkButt.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+root.geometry('500x600')
+
+frame = Frame(root)
+frame.pack()
+
+butt = Button(frame, text="Geek")
+butt.pack()
+
+root.mainloop()
---
+++ tkinter1.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter.ttk import *
+
+root = Tk()
+
+root.title("Temurbek")
+
+
+label = Label(root, text="Hello World").pack()
+
+root.mainloop()
